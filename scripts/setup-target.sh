40a1fa43d30971dd3b7719f63e96c4554e2897f6
Yurii <yurachoruk123@gmail.com>
2026-05-15 22:45:10 +0300

    fix

--- scripts/setup-target.sh
+++ scripts/setup-target.sh
@@ -12,9 +12,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyr
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add Docker Repository natively for Ubuntu
+# shellcheck source=/dev/null
+. /etc/os-release
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  $VERSION_CODENAME stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
